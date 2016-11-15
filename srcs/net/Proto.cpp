8c8703f5e1789919d4fb4539feb93c3398773e10
Amina <amina.ourouba@epitech.eu>
2016-11-15 06:07:48 +0100

    Fixing struct

---
+++ srcs/net/Proto.cpp
@@ -0,0 +1,26 @@
+#include "Proto.hh"
+
+Proto::Proto() {
+
+}
+
+Proto::~Proto() {
+}
+
+Packet getFrameProt(int prot_ID) {
+  int	i;
+
+  for (i = 0; this->_frameProt[i]. != -1; i++) {
+    // if cmp
+    // break
+  }
+  //return this->_frameProt[i]
+}
+
+Packet getTPCIPProt(int prot_ID) {
+  for (i = 0; this->_frameProt[i]. != -1; i++) {
+    // if cmp
+    // break
+  }
+  //return this->_frameProt[i]
+}
